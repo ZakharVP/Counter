20007bc2b959b1fb9bdf10dede85220d7c2a505a
Panchenko Zakhar <zakhar-panchenko@yandex.ru>
2024-06-21 00:43:56 +0300

    Создано приложение по заданию

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var i = 0
 
+    @IBOutlet weak var counterText: UILabel!
+    @IBOutlet weak var buttonCounter: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterText.text = "O"
     }
-
+    
+    
+    @IBAction func buttonClickTouchDown(_ sender: Any) {
+        i += 1
+        counterText.text = "Значение счетчика: " + String(i)
+    }
+    
 
 }
 
